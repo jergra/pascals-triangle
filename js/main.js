a24903bab91e105b98d40b3ddde5cf8fd1ffc926
jergra <jergra43@gmail.com>
2021-09-09 10:47:13 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,108 @@
+/* from pascal.html */
+function promptPromise(message) {
+    return new Promise(function(resolve, reject) {
+      var result = window.prompt(message);
+      if (result != null) {
+        resolve(result);
+      } else {
+        reject(new Error('User cancelled'));
+      }
+    });
+  }
+
+var button = document.getElementById('action');
+var buttonClear = document.getElementById('clear')
+var output = document.getElementById('prompt');
+
+button.addEventListener('click', function() {
+
+    promptPromise('Enter number of rows:')
+      .then(function(name) {
+
+        output.innerHTML = name;
+        size = name
+        
+        /* the next 5 lines are from below, not from pascal.html */
+        const initApp = (size) => {
+            console.log("size:", size)
+            const ptArray = pascalsTriangle(size);
+            domTriangle(ptArray);
+        }
+
+        initApp(size);
+        
+      })
+      .catch(function() {
+        output.innerHTML = '¯\\_(ツ)_/¯';
+      });
+  });
+
+buttonClear.addEventListener('click', function() {
+    location.reload()
+});
+
+/* end of pascal.html contribution */
+
+/*
+const initApp = (size) => {
+    const ptArray = pascalsTriangle(size);
+    domTriangle(ptArray);
+}
+document.addEventListener("DOMContentLoaded", initApp);
+*/
+
+const pascalsTriangle = (rows = 3) => {
+    if (rows < 3) rows = 3;
+    const stackArray = [];
+    let i = 1;
+    while (i <= rows) {
+
+        const rowArray = [];
+        let x = 0;
+        let rowValue;
+        while (x < i) {
+            if (!x || x === i - 1) { rowValue = 1 }
+            else {
+                rowValue =
+                    stackArray[i - 2][x] +
+                    stackArray[i - 2][x - 1];
+            }
+            rowArray.push(rowValue);
+            x++;
+        }
+        console.log(rowArray);
+        stackArray.push(rowArray);
+        i++;
+    }
+    console.log(stackArray);
+    return stackArray;
+}
+
+const domTriangle = (array) => {
+    const main = document.querySelector('main');
+
+    array.forEach(subArr => {
+        const row = buildRow(subArr);
+        main.appendChild(row);
+    })
+}
+
+const buildRow = (array) => {
+    const row = document.createElement('div');
+    row.classList.add("row");
+    array.forEach(el => {
+        const square = document.createElement('div');
+        square.classList.add("square");
+        square.textContent = el;
+        row.appendChild(square);
+    })
+    return row;
+}
+
+
+
+
+
+
+
+
---
+++ readme.txt
@@ -0,0 +1,26 @@
+sept 9, 2021
+
+C:\webdev\pascalsTriangle>
+
+this is the tutorial:
+    I Have Only One Javascript Question
+    https://www.youtube.com/watch?v=Jm4TbtO8-ko
+
+    by Dave Gray
+
+It builds a Pascal's Triangle. 
+It is an algorithm question regularily used in job interviews. 
+Dave Gray's solution is index.html.
+My solution is pascal.html. 
+Whichever solution is highlighted is the one that will run 
+when 'Go Live' (bottom right of vscode) is pressed.
+
+For my solution I borrowed some code that renders console.log output
+directly to the page. This was so that I could access the state of the
+array at each iteration in order to render that row, since otherwise I 
+could only access the array's final state at algorithm completion.
+(i.e. only the bottom row of the triangle).
+Dave Gray deals with this problem in a much better way.
+
+
+
